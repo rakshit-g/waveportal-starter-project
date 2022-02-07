3644b78697afa0c7c4fc3d1ae8e16b9573a265b6
Rakshit <rakshitgupta602@gmail.com>
2022-02-07 21:11:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,154 @@
 import * as React from "react";
+import {useEffect} from "react";
 import { ethers } from "ethers";
 import './App.css';
+import abi from "./utils/WavePortal.json";
 
 export default function App() {
 
-  const wave = () => {
-    
+
+  const [currentAccount, setCurrentAccount] = React.useState("");
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const [allWaves, setAllWaves] = React.useState([]);
+  const contractABI = abi.abi;
+  const [txn, settxn] = React.useState("");
+  const [cnt,setCount] = React.useState();
+  
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+        
+        /*
+        * Execute the actual wave from your smart contract
+        */
+        const waveTxn = await wavePortalContract.wave("this is a message");
+        console.log("Mining...", waveTxn.hash);
+        settxn(waveTxn.hash);
+        await waveTxn.wait();
+        console.log("Mined -- ", waveTxn.hash);
+        setCount(count.toNumber());
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Retrieved total wave count...", count.toNumber());
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
+  
+  const getAllWaves = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        /*
+         * Call the getAllWaves method from your Smart Contract
+         */
+        const waves = await wavePortalContract.getAllWaves();
+
+
+        /*
+         * We only need address, timestamp, and message in our UI so let's
+         * pick those out
+         */
+        let wavesCleaned = [];
+        waves.forEach(wave => {
+          wavesCleaned.push({
+            address: wave.waver,
+            timestamp: new Date(wave.timestamp * 1000),
+            message: wave.message
+          });
+        });
+
+        /*
+         * Store our data in React State
+         */
+        setAllWaves(wavesCleaned);
+      } else {
+        console.log("Ethereum object doesn't exist!")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  const checkIfWalletIsConnected = async () => {
+    /*
+    * First make sure we have access to window.ethereum
+    */
+
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+       /*
+      * Check if we're authorized to access the user's wallet
+      */
+      const accounts = await ethereum.request({method: "eth_accounts"});
+      if(accounts.length !== 0){
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+        getAllWaves();
+
+      }else {
+        console.log("No authorized account found")
+      }
+      
+    } catch (error) {
+      console.log(error);
+    }
+    
+  
+  }
+
+
+  /**
+  * Implement your connectWallet method here
+  */
+   const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+    /*
+    * This runs our function when the page loads.
+    */
+    useEffect(() => {
+      checkIfWalletIsConnected();
+    }, [])
+  
   
   return (
     <div className="mainContainer">
@@ -19,10 +161,52 @@ export default function App() {
         <div className="bio">
         I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
         </div>
+        
+        {!currentAccount && (
+          <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Connect your wallet first=">
+          Wave at me
+        </button>
+            
+        )}
 
+        {currentAccount && (
         <button className="waveButton" onClick={wave}>
           Wave at Me
         </button>
+        )}
+
+        
+        {/*
+        * If there is no currentAccount render this button
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+
+        {currentAccount && (
+          <div className="wallet-status">
+            Wallet Connected !!!
+          </div>
+        )}
+
+        {txn && (
+          <a class="btn btn-primary" href={`https://rinkeby.etherscan.io/tx/ ${txn}`} role="button">View transaction on block</a>
+        )}  
+
+        {cnt && txn && (
+          <h3> total no of waves : {cnt+1}</h3>
+        )}
+
+        {allWaves.map((wave, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px" }}>
+              <div>Address: {wave.address}</div>
+              <div>Time: {wave.timestamp.toString()}</div>
+              <div>Message: {wave.message}</div>
+            </div>)
+        })}
       </div>
     </div>
   );
